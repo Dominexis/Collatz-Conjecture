784223e52bcd7f4244eb4f56b1fae2f33817b891
dominexis <dominexis9@gmail.com>
2024-12-05 11:22:45 -0600

    Modified infinite sequence tester to test more kinds of sequences

--- infinite_sequences.py
+++ infinite_sequences.py
@@ -1,6 +1,8 @@
 import powers
 import loop_formula
+import sequence_inverter
 import trailing_bit_generator
+import p_adics
 
 
 
@@ -60,5 +62,90 @@ def normal_of_2_and_4_loop():
 
 
 
+def sequence_7_loop():
+    sequence = [2,2,4,8,16]
+    while True:
+        sequence.append(4)
+        result = loop_formula.get_loop(sequence)
+        print(f"{len(sequence)}: {large_division(result, 100)}")
+
+
+
+def test_sequence_inverter_on_loop():
+    # sequence: list[int] = [2,2,8,4]
+    sequence: list[int] = [2,2,4,8,16]
+    while True:
+        # sequence.append(2)
+        sequence.append(4)
+        result = sequence_inverter.invert_sequence(sequence, (0,1))
+        print(f"{len(sequence)}: {large_division(result, 100)}")
+
+
+
+def test_sequence_inverter_on_normal():
+    for i in range(0, len(NORMAL_SEQUENCE), 250):
+        sequence = NORMAL_SEQUENCE[:i+1]
+        result = sequence_inverter.invert_sequence(sequence)
+        print(f"{len(sequence)}: {large_division(result, 100)}")
+
+
+
+def sequence_7_trailing_bits():
+    sequence = [2,2,4,8,16]
+    values: list[tuple[int, int]] = []
+    binary_strings: list[str] = []
+    max_string_length = 0
+
+    for i in range(500):
+        sequence.append(4)
+        value = trailing_bit_generator.generate_trailing_bits(sequence, 1)
+        values.append(value)
+        binary_string = trailing_bit_generator.format_to_binary(value)
+        binary_strings.append(binary_string)
+        max_string_length = max(max_string_length, len(binary_string))
+
+    for i in range(len(sequence)):
+        binary_string = binary_strings[i]
+        print(f"{" "*(max_string_length - len(binary_string))}{binary_string}")
+
+
+
+def sequence_5_looped_2_adic():
+    sequence = [16]
+
+    for i in range(50):
+        sequence.append(4)
+        loop_result = loop_formula.get_loop(sequence)
+        division_result = p_adics.division(loop_result[0], loop_result[1])
+        print(f"{p_adics.convert_bits_to_string(division_result[0])}")
+
+
+
+def doubling_sequence_loop():
+    sequence = [2]
+    numeral = 4
+    while True:
+        increase = len(sequence)
+        for j in range(increase):
+            sequence.append(numeral)
+        result = loop_formula.get_loop(sequence)
+        print(f"{len(sequence)}: {large_division(result, 100)}")
+        numeral = 4 if numeral == 2 else 2
+
+
+
+def fours_and_twos_loop():
+    for length in range(1, 1000, 10):
+        sequence: list[int] = []
+        for i in range(length):
+            sequence.append(4)
+        for i in range(length):
+            sequence.append(2)
+        result = loop_formula.get_loop(sequence)
+        print(f"{len(sequence)}: {large_division(result, 100)}")
+
+
+
+
 if __name__ == "__main__":
-    normal_of_2_and_4_loop()
+    fours_and_twos_loop()
--- p_adics.py
+++ p_adics.py
@@ -1,22 +1,41 @@
-def get_rational_cycle(denominator: int, display: bool = False) -> list[int]:
+import powers
 
+
+
+def division(numerator: int, denominator: int, display: bool = False) -> tuple[list[int], int]:
+    
+    if numerator%2 == 0:
+        raise ValueError("Numerator must be odd")
     if denominator%2 == 0:
         raise ValueError("Denominator must be odd")
     
-    binary_string = format(denominator, "b")
+    is_negative = numerator*denominator < 0
+    numerator = abs(numerator)
+    denominator = abs(denominator)
+    
+    numerator_bits = convert_int_to_bits(numerator)
+    numerator_length = len(numerator_bits)
     if display:
-        print(f"Binary: {binary_string}")
+        print(f"Numerator: {format(numerator, "b")}")
+
+    if denominator == 1:
+        return numerator_bits, 1
+    
+    denominator_bits = convert_int_to_bits(denominator)
+    if display:
+        print(f"Denominator: {format(denominator, "b")}")
 
     bits: list[int] = [1]
     
     # Least significant bit is not included in the offset list
     offsets: list[int] = []
-    for i in range(len(binary_string)-2, -1, -1):
-        if binary_string[i] == "1":
-            offsets.append(len(binary_string) - i - 1)
+    for i in range(len(denominator_bits)-2, -1, -1):
+        if denominator_bits[i] == 1:
+            offsets.append(len(denominator_bits) - i - 1)
     if display:
         print(f"Offsets: {offsets}")
 
+    snapshot_generated = False
     cycle_snapshot: list[int] = []
     cycle_start = 0
     carry = 0
@@ -29,7 +48,8 @@ def get_rational_cycle(denominator: int, display: bool = False) -> list[int]:
         addends.append(carry)
         total = sum(addends)
 
-        if total%2:
+        # Logic for determining the next bit
+        if total%2 != (numerator_bits[i] if i < numerator_length else 0):
             total += 1
             bits.append(1)
         else:
@@ -40,19 +60,27 @@ def get_rational_cycle(denominator: int, display: bool = False) -> list[int]:
             print(f"Bits: {convert_bits_to_string(bits)}, Addends: {convert_bits_to_string(addends)}, Future bits: {convert_bits_to_string(future_bits)}")
 
 
-        if i == offsets[-1] + 1:
+        # Logic for detecting repetitions
+        if snapshot_generated and addends + future_bits == cycle_snapshot:
+            if display:
+                print("Match")
+            break
+        if i >= offsets[-1] + 1 and i >= numerator_length + 1 and not snapshot_generated:
+            snapshot_generated = True
             cycle_snapshot = addends + future_bits
             cycle_start = i + 1
             if display:
                 print(f"Snapshot generated: {convert_bits_to_string(cycle_snapshot)}")
-        if i > offsets[-1] + 1 and addends + future_bits == cycle_snapshot:
-            if display:
-                print("Match")
-            break
 
         i += 1
 
-    return bits[cycle_start:]
+
+    # Logic for negation
+    if is_negative:
+        number = powers.POWERS_OF_2[len(bits)] - convert_bits_to_int(bits)
+        bits = convert_int_to_bits(number)
+
+    return bits, cycle_start
 
 
 
@@ -62,10 +90,31 @@ def convert_bits_to_string(bits: list[int]) -> str:
         chars.append(str(bit))
     return "".join(chars)
 
+def convert_int_to_bits(number: int) -> list[int]:
+    binary_string = format(number, "b")
+    bits: list[int] = []
+    for i in range(len(binary_string)-1, -1, -1):
+        bits.append(int(binary_string[i]))
+    return bits
+
+def convert_bits_to_int(bits: list[int]) -> int:
+    number = 0
+    for i in range(len(bits)-1, -1, -1):
+        number *= 2
+        number += bits[i]
+    return number
 
 
-def prompt_single():
+
+def prompt_division():
     while True:
+        numerator = input("Numerator (leave blank to exit): ")
+        if not numerator:
+            return
+        if not numerator.isnumeric():
+            print(f"ERROR: {numerator} is not numeric!")
+            continue
+
         denominator = input("Denominator (leave blank to exit): ")
         if not denominator:
             return
@@ -73,38 +122,11 @@ def prompt_single():
             print(f"ERROR: {denominator} is not numeric!")
             continue
 
-        cycle = get_rational_cycle(int(denominator), True)
-        print(f"Cycle: {convert_bits_to_string(cycle)}\nLength: {len(cycle)}\n")
-
-
-    
-def prompt_set():
-    for denominator in range(3, 500, 2):
-        cycle = get_rational_cycle(int(denominator))
-        print(f"Denominator: {denominator}, Length: {len(cycle)}")
-
-    for power in range(2, 65):
-        denominator = int(2**power) - 1
-        cycle = get_rational_cycle(denominator)
-        print(f"Denominator: {denominator}, Length: {len(cycle)}")
-
-
-
-def prompt():
-    while True:
-        print("1) Single")
-        print("2) Set")
-        select = input("Select (leave blank to exit): ")
-
-        if not select:
-            return
-        
-        if select == "1":
-            prompt_single()
-        if select == "2":
-            prompt_set()
+        bits, cycle_start = division(int(numerator), int(denominator), True)
+        binary_string = convert_bits_to_string(bits)
+        print(f"Bits: {binary_string}\nCycle: {binary_string[cycle_start:]}, Length: {len(binary_string[cycle_start:])}\n")
 
 
 
 if __name__ == "__main__":
-    prompt()
+    prompt_division()
