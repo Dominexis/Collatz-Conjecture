0158b715672d310609879ea3fd5315554ab38ea2
Dominexis <dominexis9@gmail.com>
2025-10-18 18:41:28 +0100

    Tested various other numbers in the T sum bit builder

--- scripts/t_sum_bits.py
+++ scripts/t_sum_bits.py
@@ -47,11 +47,25 @@ def compare_bits():
     power_of_2 = 0
 
     preamble = [1, 2, 1, 1, 1, 1, 2, 2, 1, 2, 1, 1, 2, 1, 1, 1, 2, 3, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 3, 1, 1, 1, 4, 2, 2, 4, 3, 1, 1, 5, 4]
-    right_side = (modulo - 27)*3 % modulo
     loop = [2]
+    right_side = (modulo - 27)*3 % modulo
+    # preamble = [1, 2, 6, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 4, 1, 2, 1, 3, 1, 3, 1, 3, 3, 1, 1, 1, 1, 2, 7, 1, 2, 2, 3, 1, 1, 1, 1, 4, 1, 2, 1, 1, 2, 1, 1, 1, 2, 3, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 3, 1, 1, 1, 4, 2, 2, 4, 3, 1, 1, 5, 4]
+    # loop = [2]
+    # right_side = (modulo - 418379)*3 % modulo
+    # preamble = [4, 1, 3, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1, 3, 1, 6, 1, 2, 3, 1, 2, 3, 1, 1, 4, 3, 1, 3, 1, 4, 1, 1, 2, 1, 1, 3, 2, 1, 2, 1, 2, 3, 1, 1, 3, 1, 1, 3, 1, 1, 1, 2, 4, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 5, 2, 1, 1, 7, 2, 1, 4, 1, 3, 1, 2, 3, 4]
+    # loop = [2]
+    # right_side = (modulo - 48295013)*3 % modulo
     # preamble = [1, 1, 1, 4, 2, 1, 4, 1, 3, 2, 2, 3, 5, 3]
-    # right_side = (modulo - -45169)*3 % modulo
     # loop = [1, 1, 2, 1, 1, 4, 1]
+    # right_side = (modulo - -45169)*3 % modulo
+
+    # 579/5
+    # preamble = [1, 1, 2, 1, 2, 2, 3, 4, 1, 1, 1, 1, 1, 3, 2, 5, 2, 1, 1, 1]
+    # loop = [3, 1, 1]
+    # fifth = 3
+    # for i in range(300):
+    #     fifth = fifth*16 + 3
+    # right_side = fifth*579*3 % modulo
     for i in range(1000):
         t_sum_line = convert_to_bits(t_sum)
         right_side_line = convert_to_bits(right_side)
